3623419c68f421a50d52ed9a5c0d6c786c9f462a
Henry Et <exitohenry2020@icloud.com>
2019-08-03 09:33:20 -0500

    Onclick ingreso

--- app/src/main/java/com/example/et/mycartelera/Menu.java
+++ app/src/main/java/com/example/et/mycartelera/Menu.java
@@ -1,7 +1,9 @@
 package com.example.et.mycartelera;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Menu extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void onClickIncio(View target){
+        Intent inicio = new Intent(Menu.this,MainActivity.class );
+        Menu.this.startActivity(inicio);
     }
 }
